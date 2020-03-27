fbf09518cc1a1310b5ae6898406ec7350acd020b
myWsq <wsq961@outlook.com>
2020-03-27 19:35:04 +0800

    fix: 修改markdown渲染配置

--- src/utils.ts
+++ src/utils.ts
@@ -1,22 +1,24 @@
 import MarkdownIt from "markdown-it";
 
 export function renderMarkdown(text: string) {
-  const Prismjs = require("prismjs");
+  const Prism = require("prismjs");
   const loadLanguages = require("prismjs/components/");
   const md = new MarkdownIt({
     html: true,
     highlight: (str, lang) => {
-      if (!Prismjs.languages[lang]) {
+      if (lang === "css") {
+        lang = "scss";
+      }
+      if (!Prism.languages[lang]) {
         loadLanguages([lang]);
       }
+      const grammar = Prism.languages[lang];
       return (
         `<pre class="language-${lang}">` +
-        `<label data-clipboard-action="copy" class="language">${lang}</label>` +
-        `<code>${Prismjs.highlight(
-          str,
-          Prismjs.languages[lang],
-          lang
-        )}</code></pre>`
+        `<label class="language">${lang}</label>` +
+        `<code>${
+          grammar ? Prism.highlight(str, Prism.languages[lang], lang) : str
+        }</code></pre>`
       );
     }
   });
