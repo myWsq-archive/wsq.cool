eee1da43df4d3a54f982d6ec02d2e99e90781570
myWsq <wsq961@outlook.com>
2020-05-05 20:16:32 +0800

    chore: 添加百度统计

---
+++ public/baidu-stat.js
@@ -0,0 +1,7 @@
+var _hmt = _hmt || [];
+(function() {
+  var hm = document.createElement("script");
+  hm.src = "https://hm.baidu.com/hm.js?c884cddaacc7844157e6b4b9614518d0";
+  var s = document.getElementsByTagName("script")[0];
+  s.parentNode.insertBefore(hm, s);
+})();
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import App from "next/app";
 import "@/style.css";
 import React from "react";
 import { LayoutContainer } from "@/containers/Layout";
+import Head from "next/head";
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
       <LayoutContainer.Provider>
+        <Head>
+          <script src="/baidu-stat.js"></script>
+        </Head>
         <Component {...pageProps} />
       </LayoutContainer.Provider>
     );
